8c93833712725463e64bfd48b6f9764005052588
FurionCS <1115206778@qq.com>
2017-08-27 21:37:57 +0800

    项目测试

--- project/src/test/java/com/company/project/ProjectApplicationTests.java
+++ project/src/test/java/com/company/project/ProjectApplicationTests.java
@@ -1,16 +1,43 @@
 package com.company.project;
 
+import com.company.common.model.StatusCode;
+import com.company.common.response.ObjectResponse;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ProjectApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProjectApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@LocalServerPort
+	private int port;
 
+	private URL base;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Before
+	public void setUp() throws Exception {
+		this.base = new URL("http://localhost:" + port + "/");
+	}
+	@Test
+	public void testUserController() throws Exception{
+		Map map=new HashMap<>();
+		map.put("pageIdex",1);
+		map.put("pageSize",2);
+		ObjectResponse objectResponse=this.restTemplate.postForObject(this.base.toString()+"user/list", null,ObjectResponse.class,map);
+		Assert.assertEquals(objectResponse.getCode(), StatusCode.Success);
+	}
 }
