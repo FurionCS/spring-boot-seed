258ee7281e39ff12474c3bf30979f16147ef1878
FurionCS <1115206778@qq.com>
2017-09-24 14:42:50 +0800

    新增工具类，redis配置

--- project/src/main/java/com/company/project/config/CorsConfig.java
+++ project/src/main/java/com/company/project/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.company.project.config;
 
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
