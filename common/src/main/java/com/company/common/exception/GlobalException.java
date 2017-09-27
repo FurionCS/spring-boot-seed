95ebf24d5e4977d10954280cbd1491e9d6f40026
Ernest Cheng <ErnestCheng@mintcode.com>
2017-09-27 18:28:23 +0800

    新增json配置

--- common/src/main/java/com/company/common/exception/GlobalException.java
+++ common/src/main/java/com/company/common/exception/GlobalException.java
@@ -37,4 +37,6 @@ public class GlobalException extends  RuntimeException {
     }
 
 
+
+
 }
--- project/src/main/java/com/company/project/ProjectApplication.java
+++ project/src/main/java/com/company/project/ProjectApplication.java
@@ -2,8 +2,12 @@ package com.company.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableRetry
 public class ProjectApplication {
 
 	public static void main(String[] args) {
